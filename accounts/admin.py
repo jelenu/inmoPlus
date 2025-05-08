d605bc75f8dd27505c18436c3c261817305ed4bd
jelenu <jesusleon2700@gmail.com>
2025-05-08 13:35:50 +0200

    accounts, properties adminpanel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "first_name", "last_name", "role", "is_active", "is_staff")
+    list_filter = ("role", "is_active", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal Info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser")}),
+        ("Role", {"fields": ("role",)}),
+        ("Important Dates", {"fields": ("last_login",)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "first_name", "last_name", "password1", "password2", "role", "is_active", "is_staff"),
+        }),
+    )
--- properties/admin.py
+++ properties/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Property, PropertyImage
 
-# Register your models here.
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "status", "created_at")
+    list_filter = ("status", "created_at")
+    search_fields = ("title", "address")
+
+@admin.register(PropertyImage)
+class PropertyImageAdmin(admin.ModelAdmin):
+    list_display = ("property", "uploaded_at")
