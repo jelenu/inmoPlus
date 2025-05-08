f633986e40159b8209c01736dc7a251737853bd0
jelenu <jesusleon2700@gmail.com>
2025-05-08 13:44:35 +0200

    register, login, refresh token views created

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import CustomUser
+from .serializers import RegisterSerializer
 
 # Create your views here.
+
+class RegisterView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
--- inmoPlus/urls.py
+++ inmoPlus/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),  # Incluye las rutas de JWT
+
 ]
 
 if settings.DEBUG:
