05bdb86d4cfad63bc607d2acc132c6e6462fe358
jelenu <jesusleon2700@gmail.com>
2025-05-16 11:28:33 +0200

    refactor test_client_list_detail with BaseUserTestCase

--- clients/tests/test_clients_list_detail.py
+++ clients/tests/test_clients_list_detail.py
@@ -5,46 +5,29 @@ from django.urls import reverse
 from accounts.models import CustomUser
 from clients.models import Client
 from rest_framework_simplejwt.tokens import RefreshToken
-
-class ClientsListDetailTests(TestCase):
+from common_tests.base import BaseUserTestCase
+class ClientsListDetailTests(BaseUserTestCase):
     def setUp(self):
-        # * Create Admin User
-        self.admin = CustomUser.objects.create_user(
-            email="admin@test.com", password="admin123", role="admin"
-        )
-
-        # * Create Agent1 User
-        self.agent1 = CustomUser.objects.create_user(
-            email="agent1@test.com", password="agent123", role="agent"
-        )
+        super().setUp()
 
         # * Create Agent2 User
-        self.agent2 = CustomUser.objects.create_user(
-            email="agent2@test.com", password="agent123", role="agent"
+        self.agent2_user = CustomUser.objects.create_user(
+            email="agent2@test.com",
+            password="agent2password123",
+            first_name="Agent2",
+            last_name="User",
+            role="agent",
         )
 
-        # * Create Viewer User
-        self.viewer = CustomUser.objects.create_user(
-            email="viewer@test.com", password="viewer123", role="viewer"
-        )
-
-        # * Create Client with Agent1
+        # * Create Client with agent
         self.client1 = Client.objects.create(
             name="Cliente Uno",
             email="cliente1@test.com",
             phone="123456789",
             notes="Notas 1",
-            agent=self.agent1,
+            agent=self.agent,
         )
 
-        # * Create APIClient instance
-        self.api_client = APIClient()
-
-    # ! Helper function to get JWT token
-    def get_jwt_token(self, user):
-        refresh = RefreshToken.for_user(user)
-        return str(refresh.access_token)
-    
     # ! Test for Admin user accessing clients list
     def test_admin_user_access_clients_list(self):
         jwt_token = self.get_jwt_token(self.admin)
@@ -58,9 +41,9 @@ class ClientsListDetailTests(TestCase):
         self.assertIsInstance(response.data, list)
         self.assertGreater(len(response.data), 0)
 
-    # ! Test for Agent1 user accessing clients list
-    def test_agent1_user_access_clients_list(self):
-        jwt_token = self.get_jwt_token(self.agent1)
+    # ! Test for agent user accessing clients list
+    def test_agent_user_access_clients_list(self):
+        jwt_token = self.get_jwt_token(self.agent)
         self.api_client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_token}")
         
         url = reverse("client-list-create")
@@ -116,9 +99,9 @@ class ClientsListDetailTests(TestCase):
         self.assertIsInstance(response.data, dict)
         self.assertGreater(len(response.data), 0)
 
-    # ! Test for Agent1 user accessing client detail
-    def test_agent1_user_access_client_detail(self):
-        jwt_token = self.get_jwt_token(self.agent1)
+    # ! Test for agent user accessing client detail
+    def test_agent_user_access_client_detail(self):
+        jwt_token = self.get_jwt_token(self.agent)
         self.api_client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_token}")
         
         url = reverse("client-detail", args=[self.client1.id])
