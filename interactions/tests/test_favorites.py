33409cf7956ae1704d5803d2065282d75cb2f7df
jelenu <jesusleon2700@gmail.com>
2025-05-28 16:38:03 +0200

    test favorites created

---
+++ interactions/tests/test_favorites.py
@@ -0,0 +1,72 @@
+from rest_framework import status
+from django.urls import reverse
+from accounts.models import CustomUser
+from common_tests.base import BaseUserTestCase
+from interactions.models import Favorite
+from properties.models import Property
+
+class FavoritesTests(BaseUserTestCase):
+    def setUp(self):
+        super().setUp()
+
+        # * Create viewer user
+        self.viewer_user = CustomUser.objects.create_user(
+            email="testuser@example.com",
+            password="testpassword123",
+            first_name="Test",
+            last_name="User",
+        )
+        # * Create property
+        self.property = Property.objects.create(
+            title="Property 1",
+            description="Property 1 Description",
+            price=100000,
+            address="123 Property Street",
+            owner=self.agent_user,
+        )
+
+        # * Create property 2
+        self.property2 = Property.objects.create(
+            title="Property 1",
+            description="Property 1 Description",
+            price=100000,
+            address="123 Property Street",
+            owner=self.agent_user,
+        )
+
+        # * add propery to favorites
+        Favorite.objects.create(user=self.viewer_user, property=self.property)
+
+    # ! Test for viewer user adding a property to favorites
+    def test_viewer_user_add_favorite(self):
+        jwt_token = self.get_jwt_token(self.viewer_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_token}")
+
+        url = reverse("favorite-toggle", args=[self.property2.pk])
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Favorite.objects.count(), 2)
+        self.assertEqual(Favorite.objects.first().property, self.property)
+
+    # ! Test for viewer user removing a property from favorites
+    def test_viewer_user_remove_favorite(self):
+        jwt_token = self.get_jwt_token(self.viewer_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_token}")
+
+        url = reverse("favorite-toggle", args=[self.property.pk])
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Favorite.objects.count(), 0)
+
+    # ! Test for viewer user listing favorites
+    def test_viewer_user_list_favorites(self):
+        jwt_token = self.get_jwt_token(self.viewer_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_token}")
+
+        url = reverse("favorite-list")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
