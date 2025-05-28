c0d12be1b8d2bcbde227da40fbe464605a36b080
jelenu <jesusleon2700@gmail.com>
2025-05-28 16:49:40 +0200

    test_favorites updated

--- interactions/tests/test_favorites.py
+++ interactions/tests/test_favorites.py
@@ -70,3 +70,40 @@ class FavoritesTests(BaseUserTestCase):
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), 1)
+
+    # ! Test for viewer user trying to favorite a non-existent property
+    def test_viewer_user_favorite_non_existent_property(self):
+        jwt_token = self.get_jwt_token(self.viewer_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_token}")
+
+        url = reverse("favorite-toggle", args=[9999])  # Non-existent property ID
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(Favorite.objects.count(), 1)
+
+    # ! Test for unauthenticated user trying to list favorites
+    def test_unauthenticated_user_list_favorites(self):
+        url = reverse("favorite-list")
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    # ! Test for unauthenticated user trying to add a favorite
+    def test_unauthenticated_user_add_favorite(self):
+        url = reverse("favorite-toggle", args=[self.property.pk])
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        self.assertEqual(Favorite.objects.count(), 1)
+
+    # ! Test for agent user trying to add a favorite
+    def test_agent_user_add_favorite(self):
+        jwt_token = self.get_jwt_token(self.agent_user)
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {jwt_token}")
+
+        url = reverse("favorite-toggle", args=[self.property2.pk])
+        response = self.client.post(url)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(Favorite.objects.count(), 1)
