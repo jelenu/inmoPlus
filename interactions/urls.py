0a19f3877874b3cc2a5dbca1370de494a04380e2
jelenu <jesusleon2700@gmail.com>
2025-05-28 17:02:41 +0200

    Contact list url modified

--- interactions/urls.py
+++ interactions/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('favorites/', favorite_list, name='favorite-list'),
     path('favorites/<int:pk>/toggle/', favorite_toggle, name='favorite-toggle'),
     path('contact/', ContactFormCreateView.as_view(), name='contact-form-create'),
-    path('contact/list/', ContactFormListView.as_view(), name='contact-form-list'),
+    path('contact/', ContactFormListView.as_view(), name='contact-form-list'),
     path('contact/<int:pk>/', ContactFormDetailView.as_view(), name='contact-form-detail'),
     path('contact/<int:pk>/delete/', ContactFormDestroyView.as_view(), name='contact-form-delete'),
 
